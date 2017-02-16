c6dff57744fe439095ecd14409843c4b2bc90ac3
Elise St Hilaire and keely glenn <elise+keely@>
2017-02-15 16:10:03 -0800

    Create basic layout for a csharp document that uses Nancy and Razor

---
+++ Modules/HomeModule.cs
@@ -0,0 +1,17 @@
+using Nancy;
+using System.Collections.Generic;
+using Namespace.Objects;
+
+
+namespace Namespace
+{
+    public class HomeModule : NancyModule
+    {
+        public HomeModule()
+        {
+            Get["/"] = _ => {
+                return View["index.cshtml"];
+            };
+        }
+    }
+}
---
+++ Tests/ObjectTest.cs
@@ -0,0 +1,18 @@
+using Xunit;
+using Namespace.Objects;
+using System;
+using System.Collections.Generic;
+
+namespace Namespace
+{
+    public class ClassTest
+    {
+        [Fact]
+        public void methodWeAreTesting_whatBehaviorWeAreTesting_expectedResultOfTest()
+        {
+            //arrange
+            //act
+            //assert
+        }
+    }
+}
